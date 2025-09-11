d4757d02c987208c2e92ff1516ebbe2704331ea3
SoyFenixEc <soyfenixec@gmail.com>
2025-09-11 12:21:05 -0500

    change 2

--- app/src/main/java/com/agalplataformaeducativa/webview/MainActivity.kt
+++ app/src/main/java/com/agalplataformaeducativa/webview/MainActivity.kt
@@ -235,7 +235,8 @@ class MainActivity : AppCompatActivity() {
     private fun loadWebView() {
         Log.d("WebView", "loadWebView() called") // ✅ LOG 1
 
-        val subdomain = prefs.savedSubdomain ?: return
+        //val subdomain = prefs.savedSubdomain ?: return
+        val subdomain = "lapradera.agalplataformaeducativa.com"
 
         Log.d("WebView", "Loading URL: https://$subdomain") // ✅ LOG 2
 
